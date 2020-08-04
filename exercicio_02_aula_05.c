3f8afcc927585ba567e09f5ea3bb4917ef034705
WandersonGomes <dersom100@gmail.com>
2020-08-04 15:15:05 -0300

    Exercicio 02 Aula 05

---
+++ exercicio_02_aula_05.c
@@ -0,0 +1,16 @@
+/*
+Faca um programa que peca um numero e entao mostre a
+mensagem O numero informado foi [numero].
+*/
+#include <stdio.h>
+
+int main() {
+    double numero = 0.00;
+
+    printf("Informe um numero: ");
+    scanf("%lf", &numero);
+
+    printf("O numero informado foi %g.\n", numero);
+
+    return 0;
+}

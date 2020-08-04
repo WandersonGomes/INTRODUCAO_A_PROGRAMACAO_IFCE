a67a9ed3036d8002d997af912cd50abd1defe684
WandersonGomes <dersom100@gmail.com>
2020-08-04 15:18:15 -0300

    Exercicio 03 Aula 05

---
+++ exercicio_03_aula_05.c
@@ -0,0 +1,17 @@
+/*
+Faca um programa que peca dois numeros e imprima a soma.
+*/
+#include <stdio.h>
+
+int main() {
+    double numero1 = 0.00, numero2 = 0.00;
+
+    printf("Informe o primeiro numero: ");
+    scanf("%lf", &numero1);
+    printf("Informe o segundo numero: ");
+    scanf("%lf", &numero2);
+
+    printf("A soma dos dois numeros e: %g.\n", (numero1 + numero2));
+
+    return 0;
+}
